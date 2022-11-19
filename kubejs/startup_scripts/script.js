154096d5ff00376a8c05a501a72bc0f99d9b066e
Marcel Former <34310427+Selians@users.noreply.github.com>
2022-11-19 14:32:09 +0100

    Crash fix

--- kubejs/startup_scripts/script.js
+++ kubejs/startup_scripts/script.js
@@ -3,18 +3,21 @@
 console.info('KubeJS is here to fix shit)')
 
 StartupEvents.registry('item', event => {
-	event
-		.create('silverlight_raw')
-		.displayName('Silverlight')
 
-		event
-		.create('silverlight_awakened')
-		.displayName('Awakened Silverlight')
+	// ------------------ SILVERLIGHT ------------------
+
+	event
+		.create('silverlight')
+		.displayName('Silverlight');
+
+	event
+		.create('silverlight_refined')
+		.displayName('Refined Silverlight');
 	// Register new items here
 	// event.create('example_item').displayName('Example Item')
-})
+});
 
 StartupEvents.registry('block', event => {
 	// Register new blocks here
 	// event.create('example_block').material('wood').hardness(1.0).displayName('Example Block')
-})
+});
